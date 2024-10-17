cbbf3840293fbd3af8d5228a2f748b454c9c93ef
longph <longphamduy2002@gmail.com>
2024-10-17 18:33:38 +0300

    Repo init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,65 @@
+<script>
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	import { goto } from '$app/navigation';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<a class="text-xl uppercase select-none font-bold" href='/'>LONGPH.DEV</a>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn hover:variant-soft-primary"
+					href="/portfolio"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Portfolio
+				</a>
+				<a
+					class="btn hover:variant-soft-primary"
+					href="/blog"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Blog
+				</a>
+				<a
+					class="btn hover:variant-soft-primary"
+					href="https://github.com/phamduylong"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<svelte:head>
+	<title>Long Pham</title>
+</svelte:head>
+
+<div class="container h-full mx-auto flex justify-center items-center px-10">
+	<div class="space-y-16 flex flex-col items-center text-justify md:w-4/5 lg:w-3/5">
+		<h1 class="h1 font-serif text-5xl">Long Pham</h1>
+		<div>
+			<Avatar
+				src="setboiz.jpg"
+				alt="Cover photo"
+				class="float-left mr-4 h-20 md:h-36"
+				width="w-20 md:w-36"
+			/>
+			<p>
+				I'm a software engineer who enjoys solving problems and the dopamine it brings. I have a 2
+				years experience in the field, working as both a desktop app developer and a web developer
+				at
+				<a class="anchor" target="_blank" href="https://digia.com/en">Digia</a>. In my free time, I
+				fancy a bit of open source browsing, investing in tech startups and listening to tech
+				podcasts.
+			</p>
+		</div>
+	</div>
+</div>
