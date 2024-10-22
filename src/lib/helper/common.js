d124c77f1e048d2a0f900eec65c8cc6370576203
longph <longphamduy2002@gmail.com>
2024-10-23 00:36:51 +0300

    toTitleCase implementation

---
+++ src/lib/helper/common.js
@@ -0,0 +1,13 @@
+/**
+ * Transform a string to title case
+ * @param {string} str 
+ * @returns Title-cased string
+ */
+const toTitleCase = (str) => {
+    const splitStr = str.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(' ');
+};
+export { toTitleCase };
