5a75916590b48a9a16a3cdd5d541b8103f88932a
longph <longphamduy2002@gmail.com>
2024-10-25 23:08:21 +0300

    PocketBase setup

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,8 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
+import PocketBase from 'pocketbase';
+declare global {
+	declare namespace App {
+		interface Locals {
+			pb: PocketBase;
+		}
+	}
 }
--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,5 +1,15 @@
+import { DB_URL, ADMIN_USER, ADMIN_PASSWORD } from '$env/static/private';
+import PocketBase from 'pocketbase';
 export const handle = async ({ event, resolve }) => {
 	let theme = '';
+
+	event.locals.pb = new PocketBase(DB_URL);
+
+	const authData = await event.locals.pb.admins.authWithPassword(ADMIN_USER, ADMIN_PASSWORD);
+
+	console.log(authData);
+
+	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') ?? '');
 
 	const cookieTheme = event.cookies.get('theme');
 
@@ -13,6 +23,11 @@ export const handle = async ({ event, resolve }) => {
 	const response = await resolve(event, {
 		transformPageChunk: ({ html }) => html.replace('data-theme=""', `data-theme="${theme}"`)
 	});
+
+	response.headers.append(
+		'set-cookie',
+		event.locals.pb.authStore?.exportToCookie({ secure: false })
+	);
 
 	return response;
 };
