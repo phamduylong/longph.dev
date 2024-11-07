944ed4223fdeebaa4c5d3135464d5069c1beed4c
longph <longphamduy2002@gmail.com>
2024-11-08 00:15:49 +0200

    Move ts to jsdoc

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -1,11 +1,23 @@
 <script>
+	/**
+	 * @typedef {Object} Props
+	 * @property {{ title: string, content: string}} data
+	 * @property {string?} classes
+	 */
+
+	/** @type {Props} */
 	let { data, classes = '' } = $props();
-	function removeWhitespaces(str: string): string {
+
+	/**
+	 * Remove whitespaces from a string
+	 * @param str {string}
+	 */
+	function removeWhitespaces(str) {
 		return str.replace(/\s+/g, '').toLowerCase();
 	}
 </script>
 
-<div class={classes}>
+<div class={classes ? classes : ""}>
 	<h3 class="h3 anchor font-bold" id={removeWhitespaces(data.title)}>
 		<a href={'#' + removeWhitespaces(data.title)}>{data.title}</a>
 	</h3>
