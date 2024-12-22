5c962e90a85b4959d6d2358a0224b6264d5300b4
longph <longphamduy2002@gmail.com>
2024-12-23 00:02:58 +0200

    General site improvements

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -50,7 +50,7 @@
 		<h1 class="h1 font-serif text-5xl my-8 text-center">Blog</h1>
 
 		<div class="flex flex-col items-center space-y-2">
-			<InputChip bind:input={inputChip} bind:value={chosenTags} name="chips" class="max-w-sm" />
+			<InputChip placeholder="Select keywords..." bind:input={inputChip} bind:value={chosenTags} name="chips" class="max-w-sm" />
 			<div class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto" tabindex="-1">
 				<Autocomplete
 					bind:input={inputChip}
