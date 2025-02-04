d5148c7822ab7d188204a60b03f84a6eb5599701
longph <longphamduy2002@gmail.com>
2025-02-04 08:53:41 +0200

    Data update

--- src/lib/assets/data.js
+++ src/lib/assets/data.js
@@ -2,24 +2,24 @@ const section_data = Object.freeze([
 	{
 		title: 'Educations',
 		content: `I enrolled to the IT programme at <a class="anchor" href="https://www.metropolia.fi/en" target="_blank" rel="external">Metropolia UAS</a> in 2020.
-				It is the biggest college there is in Finland with an interesting curriculum so I decided to give it a shot. After 4 years of grinding,
-				I have graduated with a GPA of 4/5. My major was Embedded Systems Programming but I'm way more versatile than just an Embedded guy 😉`
+				It is the biggest college in Finland with an interesting curriculum so I decided to give it a shot. After 4 years of grinding, I have graduated with a GPA of 3.2/4.`
 	},
 	{
 		title: 'Work Experiences',
 		content: `Currently, I'm working as Junior Software Developer at <a class="anchor" href="https://digia.com/en/" target="_blank" rel="external">Digia</a>. 
 				Digia is a Finnish SaaS company that provides Financial Solutions to banks in Finland and Sweden. My main role is to develop and maintain the software 
-				according to the customers requirements. I work with <a class="anchor" href="https://learn.microsoft.com/en-us/dotnet/csharp/" target="_blank" rel="external"><i>C#</i></a>,
-				<a class="anchor" href="https://learn.microsoft.com/en-us/dotnet/visual-basic/" target="_blank" rel="external"><i>VB.NET</i></a>,
-				<a class="anchor" href="https://www.microsoft.com/en-us/sql-server/" target="_blank" rel="external"><i>SQL Server</i></a> and
-			    <a class="anchor" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="external"><i>JavaScript</i></a> on a daily basis.`
+				according to the customers requirements. I work with <a class="anchor" href="https://learn.microsoft.com/en-us/dotnet/csharp/" target="_blank" rel="external">C#</a>,
+				<a class="anchor" href="https://learn.microsoft.com/en-us/dotnet/visual-basic/" target="_blank" rel="external">VB.NET</a>,
+				<a class="anchor" href="https://www.microsoft.com/en-us/sql-server/" target="_blank" rel="external">SQL Server</a> and
+			    <a class="anchor" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="external">JavaScript</a> on a daily basis.`
 	},
 	{
 		title: 'Projects',
 		content: `I like to spend my time on side projects to learn new technologies and to keep my skills sharp. One of the most interesting open source projects I have contributed to is
-				<a class="anchor" href="https://skeleton.dev/" target="_blank" rel="external">SkeletonUI</a>. Skeleton is a fully featured UI Toolkit for building reactive interfaces quickly using 
-				<a class="anchor" href="https://svelte.dev" target="_blank" rel="external">Svelte</a> and <a class="anchor" href="https://tailwindcss.com/" target="_blank" rel="external">TailwindCSS</a>.
-				I have contributed to fixing bugs, as well as enhancing the documentation in various places. Feel free to check out <a class="anchor" href="/projects ">my personal projects page</a>.`
+				<a class="anchor" href="https://skeleton.dev/" target="_blank" rel="external">Skeleton</a>. Skeleton is a design system used to build reactive interfaces quickly using 
+				<a class="anchor" href="https://tailwindcss.com/" target="_blank" rel="external">TailwindCSS</a>. I have contributed to <a class="anchor" href="https://github.com/skeletonlabs/skeleton/pulls?q=+is%3Apr+author%3Aphamduylong+" target="_blank" rel="external">fixing bugs</a> and offer support to community members 
+				who use Skeleton for their projects. After stacking around for a while, I have joined the core team maintaining Skeleton in late 2024. Feel free to check out 
+				<a class="anchor" href="/projects">the project page</a> for more interesting projects.`
 	}
 ]);
 
