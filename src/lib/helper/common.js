8080cb71a63376f360c0b1398f614a71cb5df978
longph <longphamduy2002@gmail.com>
2025-02-21 23:23:41 +0200

    Add common function to get day of week

--- src/lib/helper/common.js
+++ src/lib/helper/common.js
@@ -10,4 +10,19 @@ const toTitleCase = (str) => {
 	}
 	return splitStr.join(' ');
 };
-export { toTitleCase };
+
+/**
+ * Convert a number to day of week
+ * @param {number} day
+ * @param {boolean} short
+ * @returns Day of week
+ */
+const toDayOfWeek = (day, short) => {
+	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	if (short) {
+		return days[day].substring(0, 3);
+	}
+	return days[day];
+};
+
+export { toTitleCase, toDayOfWeek };
